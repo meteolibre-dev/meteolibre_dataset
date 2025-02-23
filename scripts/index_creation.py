e07664be4dcd0ce495dafd348b1fa0b7253ea030
Adrien B <forbu14@gmail.com>
2025-02-23 15:45:11 +0100

    feat: add index creation

---
+++ scripts/index_creation.py
@@ -0,0 +1,33 @@
+"""
+In this python module , we create an index (parquet file)
+We simply read the list of files in data/h5 and create an index from it.
+"""
+
+import os
+
+import pandas as pd
+
+list_files = os.listdir("../data/h5")
+
+# remove non h5 files
+list_files = [f for f in list_files if f.endswith(".h5")]
+
+df = pd.DataFrame(list_files, columns=["file_path"])
+df["file_path_h5"] = "h5/" + df["file_path"]
+
+print(df.head())
+
+# data is of form T_IPRN20_C_LFPW_20250125033000.h5
+# we want to extract the date from the filename
+df["date"] = df["file_path"].str.extract(r"(\d{12})")
+
+# now we want to sort the dataframe by date
+df = df.sort_values(by="date")
+
+# preprocess the date to get datetime format
+df["datetime"] = pd.to_datetime(df["date"], format="%Y%m%d%H%M", errors="raise")
+
+# now we want to save the dataframe into a parquet file
+df.to_parquet("../data/index.parquet", index=False)
+
+
