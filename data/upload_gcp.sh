47f05d61bea9b115de5463a6e399ac2e658f720e
Adrien B <adrienbufort@gmail.com>
2025-06-25 18:31:59 +0000

    feat: update data push

--- data/upload_gcp.sh
+++ data/upload_gcp.sh
@@ -1,9 +1,11 @@
 # zip and upload file to gcp
-zip -r groundstation_npz.zip groundstation_npz/
-zip -r h5.zip h5/
+#zip -r groundstation_npz.zip groundstation_npz/
+#zip -r h5.zip h5/
+zip -r hf_dataset.zip hf_dataset/
 
-gsutil cp groundstation_npz.zip gs://meteofrance-preprocess/
-gsutil cp h5.zip gs://meteofrance-preprocess/
+#gsutil cp groundstation_npz.zip gs://meteofrance-preprocess/
+#gsutil cp h5.zip gs://meteofrance-preprocess/
+gsutil cp hf_dataset.zip gs://meteofrance-preprocess/hf_dataset_v3.zip
 
 # also push index
-gsutil cp index.parquet gs://meteofrance-preprocess/
+#gsutil cp index.parquet gs://meteofrance-preprocess/
